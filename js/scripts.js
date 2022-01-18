4301fb4a1447e94e818c1f5d8de58f28e69c09a3
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-18 10:09:18 -0800

    add form and its functionality in js
    Co-authored-by: Daniel Lindsey <daniellindsey85254@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  $("form#form").submit(function(event) {
+    
+    event.preventDefault()
+    let answers = [];
+
+    $('input').each(function() {
+      
+      answers.push($(this).val());
+    });
+
+    let answersCopy = [];
+    answersCopy.push(answers[1], answers[0],answers[2]);
+    
+    let answersCopyCopy = [];
+    answersCopyCopy = answers.concat(answersCopy);
+    console.log(answersCopyCopy);
+
+    answersCopyCopy.forEach(function(favThing) {
+      $("ul#favorite-things").prepend("<li>" + favThing + "</li>");
+    });
+    
+
+  });
+});
